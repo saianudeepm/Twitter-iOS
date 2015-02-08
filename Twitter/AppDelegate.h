2fcc2773c7009ffc3c983b3daeb19f06316438ab
saianudeepm <sai.anudeep.m@gmail.com>
2015-02-08 00:13:47 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Sai Anudeep Machavarapu on 2/8/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Sai Anudeep Machavarapu on 2/8/15.
+//  Copyright (c) 2015 salome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
